9ef5760b456897029b334423a7c5da111de1d833
VEBER Arnaud <arnaud@veber.pw>
2019-02-13 11:36:47 +0100

    feat(tf): add variables

---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "application" {
+  type = "string"
+
+  default = "www"
+}
+
+variable "domain" {
+  type = "string"
+
+  default = "granturismo.world"
+}
+
+variable "env_names" {
+  type = "map"
+
+  default = {
+    "stg" = "staging"
+    "prd" = "prod"
+  }
+}
+
+variable "env_dns_zones_prefix" {
+  type = "map"
+
+  default = {
+    "stg" = "staging."
+    "prd" = ""
+  }
+}
