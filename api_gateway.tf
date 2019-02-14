2fee144875c1329c21ef1ab91c9365bcd71b70c2
VEBER Arnaud <arnaud@veber.pw>
2019-02-14 13:31:49 +0100

    feat(apigateway): custom domain name

--- api_gateway.tf
+++ api_gateway.tf
@@ -11,3 +11,14 @@ resource "aws_api_gateway_deployment" "main" {
   rest_api_id = "${aws_api_gateway_rest_api.main.id}"
   stage_name  = "${lookup(var.env_names, terraform.workspace)}"
 }
+
+resource "aws_api_gateway_domain_name" "main" {
+  certificate_arn = "${aws_acm_certificate_validation.main.certificate_arn}"
+  domain_name     = "${var.application}.${lookup(var.env_dns_zones_prefix, terraform.workspace)}${var.domain}"
+}
+
+resource "aws_api_gateway_base_path_mapping" "main" {
+  api_id      = "${aws_api_gateway_rest_api.main.id}"
+  stage_name  = "${aws_api_gateway_deployment.main.stage_name}"
+  domain_name = "${aws_api_gateway_domain_name.main.domain_name}"
+}
