3c5493523ede956761233184185a57e477d4c29b
VEBER Arnaud <arnaud@veber.pw>
2019-02-15 17:19:13 +0100

    feat(tf): split configuration for future functions

--- iam.tf
+++ iam.tf
@@ -15,21 +15,3 @@ data "aws_iam_policy_document" "lambda_assume_role" {
     ]
   }
 }
-
-resource "aws_iam_role" "index" {
-  name = "${var.application}.${lookup(var.env_dns_zones_prefix, terraform.workspace)}${var.domain}-index"
-
-  assume_role_policy = "${data.aws_iam_policy_document.lambda_assume_role.json}"
-
-  tags = {
-    Application = "${var.application}"
-    Environment = "${lookup(var.env_names, terraform.workspace)}"
-    Workspace   = "${terraform.workspace}"
-    terraformed = "true"
-  }
-}
-
-resource "aws_iam_role_policy_attachment" "lambda_basic_execution_role" {
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-  role       = "${aws_iam_role.index.name}"
-}
